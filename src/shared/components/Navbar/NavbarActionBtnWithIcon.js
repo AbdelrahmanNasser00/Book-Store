65bdd2a65519720ce6e741d74747d53b0d3bbe0e
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2024-10-28 03:50:10 +0300

    add shadow to navbar action buttons

--- src/shared/components/Navbar/NavbarActionBtnWithIcon.js
+++ src/shared/components/Navbar/NavbarActionBtnWithIcon.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const NavbarActionBtnWithIcon = ({ icon, text, purpose, badge, onClick }) => {
   return (
-    <button className="relative mx-2 cursor-pointer rounded-full border bg-sky-50 bg-opacity-50 p-2 text-sm text-sky-800 transition-all duration-300 ease-in-out hover:bg-sky-800 hover:text-sky-50">
+    <button className="relative mx-2 cursor-pointer rounded-full bg-sky-100 bg-opacity-50 p-2 text-sm text-sky-800 shadow-sm transition-all duration-300 ease-in-out hover:bg-sky-800 hover:text-sky-50">
       {/* Icon and text */}
       <div
         onClick={(e) => {
