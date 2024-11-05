958b16ebf9acead39ab4138dafe5b39a8705b615
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2024-11-06 00:49:40 +0200

    solve navigation buttons click problem

--- src/shared/components/Navbar/NavbarActionBtnWithIcon.js
+++ src/shared/components/Navbar/NavbarActionBtnWithIcon.js
@@ -3,14 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 const NavbarActionBtnWithIcon = ({ icon, text, purpose, badge, onClick }) => {
   return (
-    <button className="relative mx-2 cursor-pointer rounded-full bg-sky-100 bg-opacity-50 p-2 text-sm text-sky-800 shadow-sm transition-all duration-300 ease-in-out hover:bg-sky-800 hover:text-sky-50">
+    <button
+      className="relative mx-2 cursor-pointer rounded-full bg-sky-100 bg-opacity-50 p-2 text-sm text-sky-800 shadow-sm transition-all duration-300 ease-in-out hover:bg-sky-800 hover:text-sky-50"
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick && onClick();
+      }}
+    >
       {/* Icon and text */}
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-          onClick && onClick();
-        }}
-      >
+      <div>
         {icon}
         {text}
       </div>
