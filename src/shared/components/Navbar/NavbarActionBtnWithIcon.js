096900b00ada2edff6a41c603119ecec9710bfd1
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2024-11-12 00:06:57 +0200

    update navigation button colors

--- src/shared/components/Navbar/NavbarActionBtnWithIcon.js
+++ src/shared/components/Navbar/NavbarActionBtnWithIcon.js
@@ -1,7 +1,7 @@
 const NavbarActionBtnWithIcon = ({ icon, text, purpose, badge, onClick }) => {
   return (
     <button
-      className="relative mx-2 cursor-pointer rounded-full border border-gray-200 p-2 font-mono text-xs text-gray-500 transition-all duration-300 ease-in-out hover:border-gray-100 hover:bg-gray-100"
+      className="relative mx-2 cursor-pointer rounded-full border border-gray-200 bg-gray-100 p-2 font-mono text-xs text-gray-500 transition-all duration-300 ease-in-out hover:border-gray-100 hover:bg-gray-200"
       onClick={(e) => {
         e.stopPropagation();
         onClick && onClick();
