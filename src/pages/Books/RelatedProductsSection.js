74d03fa10b27a1511cfd43cd09ec7a486353d805
AbdelrahmanNasser00 <abdo2652149@gmail.com>
2025-01-31 02:39:18 +0200

    Add carousel for relatedbooks

--- src/pages/Books/RelatedProductsSection.js
+++ src/pages/Books/RelatedProductsSection.js
@@ -1,17 +1,51 @@
 import React from "react";
 import BookCard from "./BookCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const RelatedProductsSection = ({ relatedProducts }) => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1280 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 1280, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 525 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 525, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div className="mt-16">
       <h2 className="mb-6 text-2xl font-semibold text-gray-800">
         Related Books
       </h2>
-      <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-5">
+      <Carousel
+        responsive={responsive}
+        swipeable={true}
+        draggable={true}
+        showDots={false}
+        infinite={false}
+        autoPlay={false}
+        keyBoardControl={true}
+        customTransition="transform 500ms ease-in-out"
+        containerClass="pb-6"
+        itemClass="px-2"
+        arrows={true}
+        renderButtonGroupOutside={true}
+      >
         {relatedProducts.map((relatedProduct) => (
           <BookCard book={relatedProduct} key={relatedProduct.bookId} />
         ))}
-      </div>
+      </Carousel>
     </div>
   );
 };
